676bfc284245ab05eacf39382cc3f7b0f7a17f8e
Enyium <123484196+Enyium@users.noreply.github.com>
2026-03-10 21:22:12 +0100

    build(commitlint): extend config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,18 @@
+const DISABLED = 0;
+const WARNING = 1;
+const ERROR = 2;
+
 export default {
     "extends": ["@commitlint/config-conventional"],
     "rules": {
-        "scope-enum": [2, "always", [
+        "scope-enum": [ERROR, "always", [
+            "commitlint",
             "insert",
             "readme",
             "ui-frameworks",
             "win-events",
         ]],
+        "body-max-line-length": [DISABLED],
+        "footer-max-line-length": [DISABLED],
     },
 }
