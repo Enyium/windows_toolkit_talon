ad5b64351e94df1ef972ebc6776d6566789a833e
Enyium <123484196+Enyium@users.noreply.github.com>
2026-03-11 15:13:53 +0100

    build(commitlint): add "gitignore" scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,6 +7,7 @@ export default {
     "rules": {
         "scope-enum": [ERROR, "always", [
             "commitlint",
+            "gitignore",
             "insert",
             "readme",
             "ui-frameworks",
